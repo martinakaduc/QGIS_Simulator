c743f6837bf83b3962d262d83de2c54c1c9d393b
Nguyen Quang Duc <nguyenquangduc2000@gmail.com>
2021-07-13 09:58:07 +0700

    Init

---
+++ agent.py
@@ -0,0 +1,38 @@
+from .status import Status
+
+class Agent():
+    def __init__(self, id, status, init_ward, geometry):
+        self._id = id
+        self._status = status
+        self._geometry = geometry
+        self._ward = init_ward
+        # histoy = (ward_id, QgsPointXY)
+        self._histories = []
+
+        self._append_history(init_ward, geometry)
+
+    def get_id(self):
+        return self._id
+
+    def set_status(self, new_status):
+        self._status = new_status
+
+    def get_status(self):
+        return self._status
+
+    def get_current_ward(self):
+        return self._ward
+
+    def get_current_geometry(self):
+        return self._geometry
+
+    def get_current_geometry_XY(self):
+        return [self._geometry.get().x(), self._geometry.get().y()]
+
+    def move(self, ward, point):
+        self._append_history(ward, point)
+
+    def _append_history(self, ward, point):
+        self._ward = ward
+        self._geometry = point
+        self._histories.append((ward, point))
---
+++ status.py
@@ -0,0 +1,6 @@
+from enum import Enum
+class Status(Enum):
+    SUSCEPTIBLE = 0
+    INFECTIOUS = 1
+    RECOVERED = 2
+    DECEASED = 3
